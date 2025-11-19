910bcb63dd22eac27ad3006e8002673f86b8bb99
Katherine <katherine.shrl@gmail.com>
2025-11-19 13:35:41 -0500

    Update - main.cpp + hashtable.h

---
+++ hashtable.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma once
+#include <vector>
+#include <list>
+#include <string>
+#include <cmath>
+#include <functional> //added in after issue with hash function
+#include "car.h"
+//using namespace std; -excluded for header file
+
+template<typename K, typename V>
+class HashTable {
+
+};
